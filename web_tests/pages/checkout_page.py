795e9a55f582e42a774dfe2c2f4cccb9f3953f4f
Ditimon01 <fonsecavinicius12@gmail.com>
2026-05-05 18:40:56 -0300

    fix(test): adicionado wait e correção de seletor no fluxo checkout

--- web_tests/pages/checkout_page.py
+++ web_tests/pages/checkout_page.py
@@ -1,5 +1,7 @@
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class CheckoutPage:
 
@@ -13,7 +15,10 @@ class CheckoutPage:
         self.driver = driver
 
     def fill(self):
-        self.driver.find_element(*self.FIRST).send_keys("João")
+        element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(self.FIRST)
+        )
+        element.send_keys("João")
         self.driver.find_element(*self.LAST).send_keys("Silva")
         self.driver.find_element(*self.ZIP).send_keys("64000-000")
         self.driver.find_element(*self.CONTINUE).click()
