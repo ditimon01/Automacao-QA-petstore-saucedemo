5cedff810637a92bce3bbbc81eff6d22872c1e04
Ditimon01 <fonsecavinicius12@gmail.com>
2026-05-05 18:25:32 -0300

    ci: adicionado pipeline para automação de testes e2e

--- conftest.py
+++ conftest.py
@@ -7,6 +7,9 @@ from selenium.webdriver.chrome.service import Service
 def driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
 
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
